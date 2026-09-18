db31778a9b44adc436a9f1592bf7f1db170abc68
Nitesh <159677546+Nitesh99390@users.noreply.github.com>
2026-09-18 17:09:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -945,4 +945,13 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # Naya event loop set karna taaki Pyrogram aur Web Server dono ek sath chal sakein
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+      
